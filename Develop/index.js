ffb407ff448fecabac4187c2ece80f7a1cbdb356
Manny Lou <mavkio1@gmail.com>
2020-08-03 00:35:54 -0400

    updated file using eslint.

--- Develop/index.js
+++ Develop/index.js
@@ -29,13 +29,13 @@ async function getUVIndex(uvi) {
   const uviElement = document.createElement('p');
   // add information to span element.
   span.innerText = uviData.current.uvi;
-  uviElement.innerText = 'UV Index: '
+  uviElement.innerText = 'UV Index: ';
   // append span-tag to p-tag
   uviElement.append(span);
   // added information to div-tag
   document.getElementById('cityInfo').append(uviElement);
 
-// If statement for UV Index Colors
+  // If statement for UV Index Colors
   if (uviData.current.uvi > 8) span.classList.add('bg-danger', 'text-white');
   else if (uviData.current.uvi > 3) span.classList.add('bg-warning');
   else span.classList.add('bg-success');
@@ -54,16 +54,16 @@ function results(info) {
   temperature.innerText = fahrenheit + ' °F';
   document.getElementById('cityInfo').append(temperature);
   // Icon for weather.
-  const iconURL = `//openweathermap.org/img/wn/${info.weather[0].icon}@2x.png`
-  const img = document.createElement('img')
+  const iconURL = `//openweathermap.org/img/wn/${info.weather[0].icon}@2x.png`;
+  const img = document.createElement('img');
   img.setAttribute('src', iconURL);
   temperature.append(img);
   // Test for fahrenheit
   console.log(fahrenheit);
   // Status for weather
   const statusText = document.createElement('p');
-  statusText.classList.add('font-weight-bold')
-  const description = info.weather[0].description
+  statusText.classList.add('font-weight-bold');
+  const description = info.weather[0].description;
   statusText.innerText = 'Current Status: ' + description.charAt(0).toUpperCase() + description.slice(1);
   document.getElementById('cityInfo').append(statusText);
   // Humidity Paragraph
@@ -74,18 +74,16 @@ function results(info) {
   const windSpeed = document.createElement('p');
   windSpeed.innerText = 'Wind Speed: ' + info.wind.speed + ' MPH';
   document.getElementById('cityInfo').append(windSpeed);
-
-
 }
 
 // Listen to Click Event to get user input
-document.getElementById('searchBtn').addEventListener('click', async function (event) {
+document.getElementById('searchBtn').addEventListener('click', async function() {
   document.getElementById('cityInfo').innerHTML = '';
   const cityName = document.getElementById('userSearch').value;
   const currentWeatherURL = `//api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=` + apiKey;
   const data = await getWeatherInformation(currentWeatherURL);
   console.log(data);
   results(data);
-  getUVIndex(data);
+  await getUVIndex(data);
 });
 
