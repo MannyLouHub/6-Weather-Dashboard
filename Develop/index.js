f81d13757aadc8c894d42313a6e19234a0c442d1
Manny Lou <mavkio1@gmail.com>
2020-08-02 22:44:20 -0400

    added comments and get UVI Index.

--- Develop/index.js
+++ Develop/index.js
@@ -1,22 +1,29 @@
 const apiKey = '588ac8f63f6e3b4105ac6b96471ea921';
-
+//get weather API information
 async function getWeatherInformation(city) {
   const data = await fetch(city);
-
   return await data.json();
 }
-
+// convert K to F function.
 function convertKelvin(temp) {
   return ((temp - 273.15) * 9 / 5 + 32).toFixed(1);
 }
-
+// Get Uv Index API Info
 async function getUVIndex(uvi) {
-  const uvIndexURL = `//api.openweathermap.org/data/2.5/onecall?lat=${uvi.coord.lat}&lon=${uvi.coord.lon}&
-exclude=hourly,daily&appid=${apiKey}`
-  const uvIndex =
+  const uvIndexURL = `//api.openweathermap.org/data/2.5/onecall?lat=${uvi.coord.lat}&lon=${uvi.coord.lon}&exclude=hourly,daily&appid=${apiKey}`;
+  const response = await fetch(uvIndexURL);
+  const uviData = await response.json();
 
+  console.log(uviData);
+  const span = document.createElement('span')
+  span.classList.add('p-2');
+  span.setAttribute('id', 'uviColor');
+  const uviElement = document.createElement('p');
+  span.innerText = 'UV Index: ' + uviData.current.uvi;
+  uviElement.append(span);
+  document.getElementById('cityInfo').append(uviElement);
 }
-
+// Applying information to first row on right side.
 function results(info) {
   const fahrenheit = convertKelvin(info.main.temp);
   const resultName = document.createElement('h2');
@@ -31,17 +38,17 @@ function results(info) {
   document.getElementById('cityInfo').append(humidity);
   const windSpeed = document.createElement('p');
   windSpeed.innerText = 'Wind Speed: ' + info.wind.speed + ' MPH'
+  document.getElementById('cityInfo').append(windSpeed);
 
 }
-
+// Listen to Click Event.
 document.getElementById('searchBtn').addEventListener('click', async function (event) {
   document.getElementById('cityInfo').innerHTML = '';
   const cityName = document.getElementById('userSearch').value;
   const currentWeatherURL = `//api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=` + apiKey;
   const data = await getWeatherInformation(currentWeatherURL);
-  const uvIndexURL  = getUVIndex(data);
-  const uvIndex =
   console.log(data);
   results(data);
+  getUVIndex(data);
 });
 
