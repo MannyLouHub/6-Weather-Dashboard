e086fd61ad27957d38bf430f4e1430a7a9850ad8
Manny Lou <mavkio1@gmail.com>
2020-08-05 15:10:25 -0400

    Updating Homework to current.

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,7 @@
 // the API Key
 const apiKey = '588ac8f63f6e3b4105ac6b96471ea921';
+//array of cities
+const cityArray = []
 
 // get weather API information
 async function getWeatherInformation(city) {
@@ -13,11 +15,10 @@ function convertKelvin(temp) {
 }
 
 // Get Uv Index API Info
-async function getUVIndex(uvi) {
-  const uvIndexURL = `//api.openweathermap.org/data/2.5/onecall?lat=${uvi.coord.lat}&lon=${uvi.coord.lon}&exclude=hourly,daily&appid=${apiKey}`;
+async function getOneCall(uvi) {
+  const uvIndexURL = `//api.openweathermap.org/data/2.5/onecall?lat=${uvi.coord.lat}&lon=${uvi.coord.lon}&exclude=hourly,minutely&appid=${apiKey}`;
   const response = await fetch(uvIndexURL);
   const uviData = await response.json();
-
   console.log(uviData);
   // create span element
   const span = document.createElement('span');
@@ -34,20 +35,59 @@ async function getUVIndex(uvi) {
   uviElement.append(span);
   // added information to div-tag
   document.getElementById('cityInfo').append(uviElement);
-
   // If statement for UV Index Colors
   if (uviData.current.uvi > 8) span.classList.add('bg-danger', 'text-white');
   else if (uviData.current.uvi > 3) span.classList.add('bg-warning');
   else span.classList.add('bg-success');
+  render5days(uviData.daily);
+}
+
+function render5days(days) {
+  for (let i = 1; i < 6; i++) {
+    // card body
+    const card = document.createElement('div')
+    card.classList.add('card', 'mx-3', 'col')
+    const cardBody = document.createElement('div')
+    cardBody.classList.add('card-body', 'text-center')
+    //date for the days
+    const date = days[i].dt
+    const currentDate = moment(+(date + '000')).format('MM/DD/YYYY');
+    // card header
+    const cardheader = document.createElement('h6');
+    cardheader.innerText = currentDate
+    // icon URL
+    const iconURL = `//openweathermap.org/img/wn/${days[i].weather[0].icon}@2x.png`
+    const img = document.createElement('img');
+    img.setAttribute('src', iconURL);
+    img.classList.add('iconImages')
+    //getting temp for days
+    const temp = convertKelvin(days[i].temp.day)
+    const para = document.createElement('div');
+    para.innerText = 'Temp: ' + temp;
+    //getting Humidity for days
+    const humidity = days[i].humidity
+    const humPara = document.createElement('div');
+    humPara.innerText = 'Humidity: ' + humidity + '%';
+//append to body
+    document.getElementById('forecast').append(card);
+    card.append(cardBody);
+    cardBody.append(cardheader);
+    cardBody.append(img);
+    cardBody.append(para);
+    cardBody.append(humPara);
+  }
 }
 
 // Applying information to first row on right side.
 function results(info) {
   // Setting convert function to variable
   const fahrenheit = convertKelvin(info.main.temp);
+  // Date
+  const date = info.dt
+  const currentDate = moment(+(date + '000')).format('MM/DD/YYYY');
   // City name Header1
   const headerName = document.createElement('h1');
-  headerName.innerText = info.name;
+  headerName.innerText = info.name + ' - ' + currentDate;
   document.getElementById('cityInfo').append(headerName);
   // Temperature Header2
   const temperature = document.createElement('h2');
@@ -77,13 +117,32 @@ function results(info) {
 }
 
 // Listen to Click Event to get user input
-document.getElementById('searchBtn').addEventListener('click', async function() {
+document.getElementById('searchBtn').addEventListener('click', async function () {
   document.getElementById('cityInfo').innerHTML = '';
   const cityName = document.getElementById('userSearch').value;
+  document.getElementById('userSearch').value = '';
   const currentWeatherURL = `//api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=` + apiKey;
   const data = await getWeatherInformation(currentWeatherURL);
+// logging object to console to view information
   console.log(data);
+// setting data to result function
   results(data);
-  await getUVIndex(data);
+//sending data to get UVindex Function
+  await getOneCall(data);
+//Creating button for dates.
+  let button = document.createElement('button');
+  button.classList.add('p-3', 'list-group-item', 'list-group-item-action"')
+  button.innerText = data.name
+  document.getElementById('buttonlist').append(button)
+
+  for (let i = 0; i < cityArray.length; i++) {
+    if (cityArray[i] === data.name) {
+      console.log(cityArray)
+      console.log('double')
+    }
+  }
+  cityArray.push(data.name);
+  console.log(cityArray);
+
 });
 
